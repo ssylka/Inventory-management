14f3c6ab109a95c5d6e7491cac5e735ee85ab2dc
Stanislav Peresypkin <stas.peresypkin@mail.ru>
2026-05-19 17:31:00 +0300

    added facebook metadata

--- obj/Debug/net8.0/Inventory Managment.AssemblyInfo.cs
+++ obj/Debug/net8.0/Inventory Managment.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory Managment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b685bf9c2eecf81bca8bb29016d9abaf6d0e09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2bbf246d312f74e035fff02a231853a9380d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory Managment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory Managment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
